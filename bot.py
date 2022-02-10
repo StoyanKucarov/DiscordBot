72a84d1536ee6e0d266a3083b1ae8784fb3e7f7e
Stel Nqkoi Edisikov <stelyo_2004@abv.bg>
2022-02-10 11:04:05 +0200

    fixed the problem with blocking

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import discord
 import re
 import datetime
 import asyncio
+import random
 from discord.ext import commands, tasks
 from discord import FFmpegPCMAudio
 from hangintherebuster import keep_alive
@@ -37,14 +38,18 @@ async def VikaiCS(ctx, user: discord.Member, vers="last"):
     # only play music if user is in a voice channel
     if voice_channel!= None:
         # create StreamPlayer
-        vc= await voice_channel.connect()
-        source=discord.FFmpegPCMAudio('audio_files/startCSLegacy.mp3')
-        if vers=="last":
-            source=discord.FFmpegPCMAudio('audio_files/startCS.mp3')
-        player=vc.play(source)
-        await asyncio.sleep(3)
-        # disconnect after the player has finished
-        await vc.disconnect()
+        try:
+            vc= await voice_channel.connect()
+            source=discord.FFmpegPCMAudio('audio_files/startCSLegacy.mp3')
+            if vers=="last":
+                source=discord.FFmpegPCMAudio('audio_files/startCS.mp3')
+            player=vc.play(source)
+            await asyncio.sleep(3)
+            # disconnect after the player has finished
+            await vc.disconnect()
+        except Exception as e:
+            print(str(e))
+            await ctx.send('Error in connecting')
     else:
         await ctx.send('User is not in a channel.')
 
@@ -120,6 +125,19 @@ def makeBotName(name):#make bot name ex:Stelyo -> b0tlyo
         return "{} b0t"+name[2:]
     else:
         return "{} b0t"+name[3:]
+
+@client.event
+async def on_message(message):
+  chance=random.random()
+  if chance<=0.02 and message.author.id!=940642505953796106:
+    await message.channel.send("tiho e slow")
+  await client.process_commands(message)
+
+def IsVasilenRandom():
+    isTwo=random.randint(1, 100)
+    if isTwo<=2:
+        return True
+    return False
 
 BotToken = codes.token #change to os.environ['token'] 
 keep_alive()
